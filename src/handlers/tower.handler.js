3060c1596931ba28ee3a409e9f8c157f79d1bbe0
liha-engineer <liha.engineer@gmail.com>
2024-11-11 16:27:03 +0900

    Fix : 타워구입 핸들러 enemy -> opponent로 수정 및 타워 공격 핸들러 초안 작성 - #8

--- src/handlers/tower.handler.js
+++ src/handlers/tower.handler.js
@@ -1,6 +1,6 @@
 import { PACKET_TYPES } from "../constants/packetTypes.js";
 import { getServerGameAssets } from "../init/assets.js";
-import { getGameSessions } from "../session/game.session.js";
+import { getGame, getGameSessions } from "../session/game.session.js";
 import { getUserBySocket } from "../session/user.session.js";
 import { createResponse } from "../utils/response/createResponse.js";
 
@@ -23,11 +23,11 @@ export const towerPurchaseHandler = async ({ socket, userId, payload }) => {
     
     const gameSession = getGameSessions().find((game) => game.users.includes(user));
     
-    const enemyId = gameSession.getOpponentUserId(user.id)
-    const enemy = gameSession.getUser(enemyId)
+    const opponentId = gameSession.getOpponentUserId(user.id)
+    const opponent = gameSession.getUser(opponentId)
 
     // 이때 잡아오는건 적의 소켓을 잡아와서 bindTower에 넣어줘야 적 소켓에다 노티를 쏠 수 있다
-    const newTower = user.bindTower(enemy.socket, x, y);
+    const newTower = user.bindTower(opponent.socket, x, y);
     
     const towerPurchaseResponse = createResponse(PACKET_TYPES.TOWER_PURCHASE_RESPONSE, {
         towerId: newTower.id
@@ -36,7 +36,35 @@ export const towerPurchaseHandler = async ({ socket, userId, payload }) => {
     socket.write(towerPurchaseResponse);
 }
 
+// 
 export const attackEnemeyHandler = async ({ socket, userId, payload }) => {
+    const { towerId, monsterId } = payload;
+    const user = getUserBySocket(socket)
+    const gameSession = getGame(user.gameId)
+
+    const userTower = user.towers.find((tower) => tower.id === towerId);
+    if (!userTower) {
+      console.log('타워가 존재하지 않습니다');
+    }
+  
+    const userMonster = user.monsters.find((monster) => monster.id === monsterId);
+    if (!userMonster) {
+      console.log('몬스터가 존재하지 않습니다');
+    }
     
+    let dieMonster;
+    // hp < 0 이면 false를 반환
+    const monsterAlive = userTower.attack(userMonster)
+    if (!monsterAlive) {
+        const monsterIndex = userMonster.getMonsterIndex(monsterId)
+        dieMonster = userMonster.removeMonster(monsterIndex)
+    }
+
+    const attackEnemyReponse = createResponse(PACKET_TYPES.ENEMY_TOWER_ATTACK_NOTIFICATION, {
+        towerId,
+        monsterId
+    })
+
+    socket.write(attackEnemyReponse)
 
 }
