55482daf561e0ef86c0a39ec6b851fc49ba3bca7
liha-engineer <liha.engineer@gmail.com>
2024-11-11 16:25:40 +0900

    Fix : Tower 클래스 변수 및 메서드 수정

--- src/classes/models/tower.class.js
+++ src/classes/models/tower.class.js
@@ -1,25 +1,24 @@
+import towertable from '../../../assets/tower.json' with { type : json }
+
 class Tower {
-  constructor(x, y, id) {
-    this.id = id;
+  constructor(x, y, index) {
     this.x = x;
     this.y = y;
-    this.beamDuration = 0;
-    this.tartget = null;
+    this.towerdata = towertable.data[0]
+    this.id = towerdata.id
+    this.type = towerdata.type
+    this.cost = towerdata.cost
+    this.power = towerdata.attack_power
+    this.range = towerdata.range
+    this.cooldown = towerdata.cooldown
+    this.beamDuration = towerdata.beamDuration
+    this.upgradeCost = towerdata.upgradeCost;
   }
 
   attack(monster) {
-    if (this.cooldown <= 0) {
-      monster.hp -= this.attackPower; // 몬스터의 HP 감소
-      this.cooldown = 180; // 3초  60 당 1 초
-      this.beamDuration = 30; // 광선 지속 시간
-      this.target = monster; // 광선의 목표 설정
-    }
-  }
-
-  updateCooldown() {
-    if (this.cooldown > 0) {
-      this.cooldown--;
-    }
+    // 몬스터 hp < 0이면 false, 아니면 true 반환할 것 
+    const monsterAlive = monster.attacked(this.power)
+    return monsterAlive;
   }
 }
 
